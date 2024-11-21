8fdf2879b90ae548c0c2f03fe30e3bc4b59e46ac
SeckMohameth <mohamethseck34@gmail.com>
2024-11-20 19:45:02 -0500

    Initial Commit

---
+++ MoThoughts/MoThoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoThoughtsApp.swift
+//  MoThoughts
+//
+//  Created by Mohameth Seck on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MoThoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
